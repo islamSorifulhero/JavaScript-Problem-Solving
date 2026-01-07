16eb263db713c4c25630c99330fb68374cbc95e5
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-01-07 23:25:48 +0600

    Problem 2: Count Vowels in a String

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Problem 1: Reverse a String
+
 function reverseString(str) {
   let reversed = "";
   for (let i = str.length - 1; i >= 0; i--) {
@@ -6,6 +8,22 @@ function reverseString(str) {
   return reversed;
 }
 
-// Example
-console.log(reverseString("hello"));
-// olleh
+console.log(reverseString("hello")); // olleh
+
+
+// Problem 2: Count Vowels in a String
+
+function countVowels(str) {
+  let count = 0;
+  let vowels = "aeiou";
+
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i].toLowerCase())) {
+      count++;
+    }
+  }
+  return count;
+}
+
+console.log(countVowels("programming")); // 3
+
