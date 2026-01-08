10e875220219c3ce2c79440dedb7ab7ffadb367b
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-01-08 07:32:39 +0600

    Problem 10: PingPong Challenge

--- index.js
+++ index.js
@@ -134,3 +134,23 @@ function factorial(num) {
 }
 
 console.log(factorial(5)); // 120
+
+
+// Problem 10: PingPong Challenge
+
+function pingPong() {
+  for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("PingPong");
+    } else if (i % 3 === 0) {
+      console.log("Ping");
+    } else if (i % 5 === 0) {
+      console.log("Pong");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+// Run
+pingPong();
