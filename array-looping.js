aeec7cf455284b8dd458473df8d8e6e3d1497f65
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-04-06 23:06:15 +0600

    Array Looping method

--- array-looping.js
+++ array-looping.js
@@ -1,29 +1,56 @@
-const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange'];
 
-let reversed = [];
+// let reversed = [];
 
-for (let i = colors.length - 1; i >= 0; i--) {
-    reversed.push(colors[i]);
-}
+// for (let i = colors.length - 1; i >= 0; i--) {
+//     reversed.push(colors[i]);
+// }
+// console.log(reversed);
 
-console.log(reversed);
+
+//2///////////////
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+
+// let evenNumbers = [];
+
+// for (let i = 0; i <= numbers.length; i++) {
+//     if (numbers[i] % 2 === 0) {
+//         evenNumbers.push(numbers[i]);
+//     }
+// }
+// console.log(evenNumbers);
 
 
-const numbers = [12, 98, 5, 41, 23, 78, 46];
 
-let evenNumbers = [];
+//3///////////
+// var number = ['Tom', 'Tim', 'Tin', 'Tik']
 
-for (let i = 0; i <= numbers.length; i++) {
-    if (numbers[i] % 2 === 0) {
-        evenNumbers.push(numbers[i]);
-    }
-}
-console.log(evenNumbers);
+// let result = [];
+// for (let i of number) {
+//     result = result + i
+// }
+// console.log(result);
 
-var number = ['Tom', 'Tim', 'Tin', 'Tik']
 
-let result = [];
-for (let i of number) {
-    result = result + i
-}
-console.log(result);
+//04/////////////
+// const statement = 'I am a hard working person';
+
+// let words = statement.split(' ');
+// let reversed = [];
+
+// for (let i = words.length - 1; i >= 0; i--){
+//     reversed.push(words[i]);
+// }
+// let result = reversed.join(' ')
+// console.log(result);
+
+
+// //05/////////////////////
+// const original = [1, 2, 3];
+// const copy = [...original];
+
+// copy[0] = 99;
+
+// console.log(original);
+// console.log(copy);
+
