ad84b0ea3c494d4885182a889315d970c4b12594
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-04-16 23:43:05 +0600

    Train TT's Fine Calculator

--- functionTask.js
+++ functionTask.js
@@ -7,10 +7,21 @@
 
 
 
-function totalFine(fine){
-    let result = fine + 20;
+// function totalFine(fine){
+//     let result = fine + 20;
+//     return result;
+// }
+
+// const result = totalFine(405);
+// console.log(result); // 425
+
+
+// Task 01///////////////////
+function totalFine(fare) {
+    if (typeof fare !== "number" || fare <= 0) {
+        return 'Invalid';
+    }
+    const result = fare + (fare * 20 / 100) + 30;
     return result;
 }
-
-const result = totalFine(405);
-console.log(result); // 425
+console.log(totalFine(200));
