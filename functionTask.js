c317b093d0affc03f8b6b6bb0a81c0f57ab65abb
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-04-18 20:34:44 +0600

    FIFA Best Team Award

--- functionTask.js
+++ functionTask.js
@@ -27,16 +27,45 @@
 // console.log(totalFine(200));
 
 
-function onlyCharacter(str) {
+// function onlyCharacter(str) {
+//     // validation
+//     if (typeof str !== "string") {
+//         return "Invalid";
+//     }
+
+//     // remove spaces + uppercase
+//     let result = str.split(" ").join("").toUpperCase();
+
+//     return result;
+// }
+// const result = onlyCharacter('Hello we have a plain');
+// console.log(result);
+
+
+
+function bestTeam(player1, player2) {
     // validation
-    if (typeof str !== "string") {
+    if (typeof player1 !== "object" || typeof player2 !== "object") {
         return "Invalid";
     }
 
-    // remove spaces + uppercase
-    let result = str.split(" ").join("").toUpperCase();
+    // total calculation
+    let total1 = player1.foul + player1.cardY + player1.cardR;
+    let total2 = player2.foul + player2.cardY + player2.cardR;
 
-    return result;
+    // compare
+    if (total1 < total2) {
+        return player1.name;
+    } else if (total2 < total1) {
+        return player2.name;
+    } else {
+        return "Tie";
+    }
 }
-const result = onlyCharacter('Hello we have a plain');
-console.log(result);
+
+console.log(
+    bestTeam(
+        { name: "Germany", foul: 30, cardY: 1, cardR: 1 },
+        { name: "France", foul: 20, cardY: 2, cardR: 1 }
+    )
+);
