86d7a317d66161245b33d93a1ebcfdce1bc05f0d
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-04-20 19:53:05 +0600

    Find Highest Scorer

--- array.js
+++ array.js
@@ -1,6 +1,31 @@
-let tourist = ['cox-bazar', 'sajek', 'bandorban'];
-tourist.push('chottogram');
-tourist.push('Dhaka', 'Shylet');
+// let tourist = ['cox-bazar', 'sajek', 'bandorban'];
+// tourist.push('chottogram');
+// tourist.push('Dhaka', 'Shylet');
 
-tourist.pop();
-console.log(tourist);
+// tourist.pop();
+// console.log(tourist);
+
+
+// Problem: Find Highest Scorer
+function highestScorer(students) {
+    if (!Array.isArray(students)) {
+        return "Invalid";
+    }
+    let maxMarks = -Infinity;
+    let topper = "";
+
+    for (let student of students) {
+        if (student.marks > maxMarks) {
+            maxMarks = student.marks;
+            topper = student.name;
+        }
+    }
+    return topper
+}
+console.log(
+    highestScorer([
+        { name: "Rahim", marks: 75 },
+        { name: "Karim", marks: 85 },
+        { name: "Salam", marks: 80 }
+    ])
+); // Karim
