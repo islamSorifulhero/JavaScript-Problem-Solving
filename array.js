3738693b126cd02d9ea2eb16136fb0705eb82301
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-04-21 22:15:39 +0600

    Remove Duplicate Numbers

--- array.js
+++ array.js
@@ -6,26 +6,41 @@
 // console.log(tourist);
 
 
-// Problem: Find Highest Scorer
-function highestScorer(students) {
-    if (!Array.isArray(students)) {
-        return "Invalid";
-    }
-    let maxMarks = -Infinity;
-    let topper = "";
+// // Problem: Find Highest Scorer
+// function highestScorer(students) {
+//     if (!Array.isArray(students)) {
+//         return "Invalid";
+//     }
+//     let maxMarks = -Infinity;
+//     let topper = "";
 
-    for (let student of students) {
-        if (student.marks > maxMarks) {
-            maxMarks = student.marks;
-            topper = student.name;
+//     for (let student of students) {
+//         if (student.marks > maxMarks) {
+//             maxMarks = student.marks;
+//             topper = student.name;
+//         }
+//     }
+//     return topper
+// }
+// console.log(
+//     highestScorer([
+//         { name: "Rahim", marks: 75 },
+//         { name: "Karim", marks: 85 },
+//         { name: "Salam", marks: 80 }
+//     ])
+// ); // Karim
+
+
+function removeDuplicate(arr) {
+    if (!Array.isArray(arr)) {
+        return "Invalid"
+    }
+    let unique = [];
+    for (let num of arr) {
+        if (!unique.includes(num)) {
+            unique.push(num)
         }
     }
-    return topper
+    return unique;
 }
-console.log(
-    highestScorer([
-        { name: "Rahim", marks: 75 },
-        { name: "Karim", marks: 85 },
-        { name: "Salam", marks: 80 }
-    ])
-); // Karim
+console.log(removeDuplicate([1, 2, 2, 3, 4, 4, 5]));
