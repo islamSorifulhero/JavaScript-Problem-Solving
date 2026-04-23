a20f4e91a1484619acc7e8d3a7714cefac1e5a13
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-04-23 17:55:25 +0600

    Problem: Shopping Cart Total

--- array.js
+++ array.js
@@ -47,35 +47,61 @@
 
 
 
-// Problem: Password Validator
-function isValidPassword(password) {
-    if (typeof password !== "string") {
-        return "Invalid";
-    }
-    if (password.length < 8) {
-        return "Invalid"
-    }
-    let hasUpper = false;
-    let hasLower = false;
-    let hasNumber = false;
+// // Problem: Password Validator
+// function isValidPassword(password) {
+//     if (typeof password !== "string") {
+//         return "Invalid";
+//     }
+//     if (password.length < 8) {
+//         return "Invalid"
+//     }
+//     let hasUpper = false;
+//     let hasLower = false;
+//     let hasNumber = false;
 
-    for (let char of password) {
-        if (char >= 'A' && char <= "Z") {
-            hasUpper = true;
-        }
-        else if (char >= 'a' && char <= 'z') {
-            hasLower = true;
-        }
-        else if (char >= '0' && char <= '9') {
-            hasNumber = true;
-        }
-    }
-    if (hasUpper && hasLower && hasNumber) {
-        return 'Valid';
-    }
-    else {
+//     for (let char of password) {
+//         if (char >= 'A' && char <= "Z") {
+//             hasUpper = true;
+//         }
+//         else if (char >= 'a' && char <= 'z') {
+//             hasLower = true;
+//         }
+//         else if (char >= '0' && char <= '9') {
+//             hasNumber = true;
+//         }
+//     }
+//     if (hasUpper && hasLower && hasNumber) {
+//         return 'Valid';
+//     }
+//     else {
+//         return "Invalid";
+//     }
+// }
+// console.log(isValidPassword("Pass1234"));
+// console.log(isValidPassword('password'));
+
+
+function cartTotal(cart) {
+    // validation
+    if (!Array.isArray(cart)) {
         return "Invalid";
     }
+
+    let total = 0;
+
+    for (let item of cart) {
+        if (typeof item.price !== "number" || typeof item.quantity !== "number") {
+            return "Invalid";
+        }
+
+        total += item.price * item.quantity;
+    }
+
+    return total;
 }
-console.log(isValidPassword("Pass1234"));
-console.log(isValidPassword('password'));
+console.log(
+  cartTotal([
+    { name: "Shirt", price: 500, quantity: 2 },
+    { name: "Pant", price: 1000, quantity: 1 }
+  ])
+); // 2000
